c9d019d8386fdaf96ef1b6e87684a16e6fe7b13d
Denis <Tennis@DESKTOP-DVM9D1E>
2018-02-09 17:49:02 +0200

    Initial commit

---
+++ Factorial/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Factorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while(true)
+            {
+
+
+                Console.WriteLine("Введите число. Вычислить факториал числа");
+                int factorial = Convert.ToInt32(Console.ReadLine());
+                int a = factorial;
+                int answer = 1;
+                for (int i = 1; i <= factorial; i++)
+                {
+                    answer = answer * i;
+                }
+                Console.WriteLine("Факториал " + factorial + " это " + answer);
+
+            }
+            
+        }
+    }
+}
+
